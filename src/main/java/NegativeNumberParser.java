450c51f2ce0475fc9b095ac71391c9db493dabb8
itamar-marom <marom.itamar@gmail.com>
2022-02-06 00:00:50 +0200

    better

--- src/main/java/NegativeNumberParser.java
+++ src/main/java/NegativeNumberParser.java
@@ -5,40 +5,40 @@ public class NegativeNumberParser implements Parser {
     public String parse(HashMap<String, Double> variables, String str) throws Exception {
 
         char[] arr = str.toCharArray();
-        String newEquation = "";
+        StringBuilder newEquation = new StringBuilder();
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == '-') {
                 if ((i > 0) && ((Character.isDigit(arr[i - 1])) || (arr[i - 1] == ')'))) {
-                    newEquation += arr[i];
+                    newEquation.append(arr[i]);
                 } else if (((i + 1) < arr.length) && (arr[i + 1] == '(')) {
                     if (i == 0)
-                        newEquation += "(0-1)*";
+                        newEquation.append("(0-1)*");
                     else
-                        newEquation += arr[i] + "1*";
+                        newEquation.append(arr[i]).append("1*");
                 } else {
-                    String number = "";
+                    StringBuilder number = new StringBuilder();
                     int numberEndIndex = i + 1;
 
                     // Get the entire number
                     while (numberEndIndex < arr.length) {
                         if ((!Character.isDigit(arr[numberEndIndex])) && ((arr[numberEndIndex]) != '.'))
                             break;
-                        number += arr[numberEndIndex];
+                        number.append(arr[numberEndIndex]);
                         numberEndIndex++;
                     }
 
-                    if (number.isEmpty())
+                    if (number.length() == 0)
                         throw new Exception("Expression cannot end with an operator.");
 
-                    newEquation += "(0-" + number + ")";
+                    newEquation.append("(0-").append(number).append(")");
                     i = numberEndIndex - 1;
                 }
             } else {
-                newEquation += arr[i];
+                newEquation.append(arr[i]);
             }
         }
 
-        return newEquation;
+        return newEquation.toString();
     }
 }
